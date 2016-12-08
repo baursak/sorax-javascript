2bdc2ad676d169805b21e6f2cc5e95560d9baaa8
baursak <baursak@gmail.com>
2016-12-08 07:34:33 +0300

    endn of lesson 02: string methods

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log('Hello world');
+console.log("hello, world".length);
+console.log("Another \
+very \
+long \
+string");
+console.log("hello, " + "world");
+var str = "Sometimes the same is different";
+console.log(str.charAt(0));
+console.log(str.length);
+console.log(str.charAt(str.length - 1));
+console.log(str.substring(10, str.length));
+console.log(str.substr(10, 3));
+console.log(str.slice(-10));
+console.log(str.indexOf("i"));
+console.log(str.lastIndexOf("i"));
+console.log(str.replace("is", "is not"));
+console.log(str);
+console.log(str.split(" "));
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str[4]);
