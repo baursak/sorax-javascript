ead6814fbfb0054d9de191ebcee19f97f09eadca
baursak <baursak@gmail.com>
2016-12-09 08:41:00 +0300

    mid of lesson 17: objects can use functions as props

--- main.js
+++ main.js
@@ -1,16 +1,11 @@
 var person = {
     name: "sorax",
     age: 20,
-    gender: "male"
+    gender: "male",
+    // it's possible to use function as prop
+    sayHi: function(){
+        return "Hello";
+    }
 };
-console.log(person.name);
-console.log(person["age"]); // the same as above
-console.log(person.age);
-console.log(person.gender);
 
-// it's possible to update object props
-person["age"] = 25;
-
-// it's possible to add props
-person.userId = 1;
-console.log(person);
+console.log(person.sayHi());
