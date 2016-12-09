273c8ebf30f132d284a91800c85f87c9a062e2f5
baursak <baursak@gmail.com>
2016-12-09 09:08:57 +0300

    mid of lesson 17: props deletion

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-var object = Object.create({ x: 10, y: 20});
+/*var object = Object.create({ x: 10, y: 20});  -  in this case delete operator doesn't work*/
+// but using object creation via object literal - deletion works
+var object = {
+    x: 10,
+    y: 20
+};
 console.log(object.x);
 console.log(object.y);
+delete object["x"];
+console.log(object.x);
